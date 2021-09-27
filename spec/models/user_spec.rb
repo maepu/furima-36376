7378f4a565c5b00cff6fd25caa26b1f131d6c202
maepu <88304662+maepu@users.noreply.github.com>
2021-09-27 16:44:05 +0900

    単体テストコード　一応完了

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,55 +10,42 @@ RSpec.describe User, type: :model do
       it 'nameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
         expect(@user).to be_valid
       end
-    end
-    it 'nameが6文字以下であれば登録できる' do
-
-    end
-    it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
-      @user.password = '123456'
-      @user.password_confirmation = '123456'
-      expect(@user).to be_valid
-    end
-    it 'nameが空では登録できない' do
-      @user.name = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Name can't be blank")
-    end
-    it 'emailが空では登録できない' do
-      @user.email = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Email can't be blank")
-    end
-    it 'passwordが空では登録できない' do
-      @user.password = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password can't be blank")
-    end
-    it 'passwordとpassword_confirmationが不一致では登録できない' do
-      @user.password = '123456'
-      @user.password_confirmation = '1234567'
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
-    end
-    it 'nicknameが7文字以上では登録できない' do
-    end
-    it '重複したemailが存在する場合登録できない' do
-    end
-    it 'passwordが5文字以下では登録できない' do
-      @user.password = '12345'
-      @user.password_confirmation = '12345'
-      @user.valid?
-      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
-    end
-    it 'last_name_chinese_charactersが空では登録できない' do
-      @user.last_name_chinese_characters = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Last_name_chinese_characters can't be blank")
-    end
-    it 'last_name_katakanaが空では登録できない' do
-      @user.last_name_katakana = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Last_name_katakana can't be blank")
+      it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
+        @user.password = '123456'
+        @user.password_confirmation = '123456'
+        expect(@user).to be_valid
+      end
+      it 'emailが空では登録できない' do #メールアドレスが必須であること
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation = '1234567'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'nameが7文字以上では登録できない' do
+        @user.name = '1234567'
+      end
+      it '重複したemailが存在する場合登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = '12345'
+        @user.password_confirmation = '12345'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
     end
   end
 end
